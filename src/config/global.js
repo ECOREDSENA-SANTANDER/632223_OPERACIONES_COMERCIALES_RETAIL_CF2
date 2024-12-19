4612ab805055422312167d101a264f329041734c
PABG1990 <110750475+PABG1990@users.noreply.github.com>
2024-12-19 11:30:25 -0500

    Ajustes

--- src/config/global.js
+++ src/config/global.js
@@ -149,7 +149,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/CF2_632223_DU.pdf',
       },
       {
         icono: 'fas fa-download',
